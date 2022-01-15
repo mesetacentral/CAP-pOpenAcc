d5938887a050c841766bd8ddb92694c63aa958a6
Ignacio Cobas <ignaciocobas@outlook.com>
2022-01-16 00:05:49 +0100

    changes

--- p2.c
+++ p2.c
@@ -7,13 +7,13 @@
  *
  * (c) 2019 Arturo Gonzalez Escribano
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <float.h>
-#include <sys/time.h>
-#include <openacc.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<float.h>
+#include<sys/time.h>
+#include<omp.h>
 
 /* Function to get wall time */
 double cp_Wtime(){
@@ -25,6 +25,8 @@ double cp_Wtime(){
 #define RADIUS_TYPE_1		3
 #define RADIUS_TYPE_2_3		9
 #define THRESHOLD	0.1f
+#define NUM_THREADS 8
+
 
 /* Structure to store data of an extinguishing team */
 typedef struct {
@@ -115,7 +117,10 @@ int main(int argc, char *argv[]) {
 
 	// Simulation data
 	int rows, columns, max_iter;
-	float *restrict surface, *restrict surfaceCopy;
+	float *restrict surface;
+	float *restrict surfaceCopy;
+//	float *surface;
+//	float *surfaceCopy;
 	int num_teams, num_focal;
 	Team *teams;
 	FocalPoint *focal;
@@ -141,7 +146,7 @@ int main(int argc, char *argv[]) {
 		if ( args == NULL ) {
 			fprintf(stderr,"-- Error in file: not found: %s\n", argv[2]);
 			exit( EXIT_FAILURE );
-	      	}
+		}	
 
 		/* 1.2.2. Read surface and maximum number of iterations */
 		int ok;
@@ -292,93 +297,100 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr,"-- Error allocating: surface structures\n");
 		exit( EXIT_FAILURE );
 	}
-	for( i=0; i<rows; i++ )
+
+	int flag_stability = 0, first_activation = 0, num_deactivated = 0, iter, iter_total, x, y, target, radius, step;
+	//float global_residual = 0.0f;
+	float distance, local_distance, dx, dy;
+
+//	#pragma acc data copyout(surface, surfaceCopy)
+	#pragma acc loop gang vector collapse(2)
+	for( i=0; i<rows; i++ ) {
 		for( j=0; j<columns; j++ ) {
 			accessMat( surface, i, j ) = 0.0;
 			accessMat( surfaceCopy, i, j ) = 0.0;
 		}
-
+	}
 	/* 4. Simulation */
-	int iter;
-	int flag_stability = 0;
-	int first_activation = 0;
 	for( iter=0; iter<max_iter && ! flag_stability; iter++ ) {
 
 		/* 4.1. Activate focal points */
-		int num_deactivated = 0;
+		num_deactivated = 0;
+		#pragma acc loop seq
 		for( i=0; i<num_focal; i++ ) {
 			if ( focal[i].start == iter ) {
 				focal[i].active = 1;
 				if ( ! first_activation ) first_activation = 1;
 			}
-			// Count focal points already deactivated by a team
-			if ( focal[i].active == 2 ) num_deactivated++;
 		}
 
-		/* 4.2. Propagate heat (10 steps per each team movement) */
-		float global_residual = 0.0f;
-		int step;
-		#pragma acc data copy(surface[0:rows][0:columns], surfaceCopy[0:rows][0:columns])
-		{
+		if (first_activation) {
+			//#pragma acc data copy(num_deactivated) copyin(focal[:num_focal])
+			#pragma acc loop gang vector reduction(+:num_deactivated)
+			for( i=0; i<num_focal; i++ )
+				// Count focal points already deactivated by a team
+				num_deactivated += focal[i].active == 2;
+
+			/* 4.2. Propagate heat (10 steps per each team movement) */
 			for( step=0; step<10; step++ )	{
 				/* 4.2.1. Update heat on active focal points */
-				#pragma parallel loop
-				{
-					for( i=0; i<num_focal; i++ ) {
-						if ( focal[i].active != 1 ) continue;
-						int x = focal[i].x;
-						int y = focal[i].y;
-						if ( x < 0 || x > rows-1 || y < 0 || y > columns-1 ) continue;
-						accessMat( surface, x, y ) = focal[i].heat;
+				//#pragma acc data copyin(focal[:num_focal]) copy(surface)
+				#pragma acc loop seq
+				for( i=0; i<num_focal; i++ ) {
+					if ( focal[i].active != 1 ) continue;
+					x = focal[i].x;
+					y = focal[i].y;
+					if ( x < 0 || x > rows-1 || y < 0 || y > columns-1 ) continue;
+					accessMat( surface, x, y ) = focal[i].heat;
+				}
+				/* 4.2.2. Copy values of the surface in ancillary structure (Skip borders) */
+				//#pragma acc data copyout(surfaceCopy[:]) copyin(surface[:])
+				#pragma acc loop seq
+				for( i=1; i<rows-1; i++ ) {
+					#pragma acc loop gang vector
+					for( j=1; j<columns-1; j++ ) {
+						accessMat( surfaceCopy, i, j ) = accessMat( surface, i, j );
 					}
 				}
-
-				/* 4.2.2. Copy values of the surface in ancillary structure (Skip borders) */
-				#pragma parallel loop
-				{
-					for( i=1; i<rows-1; i++ )
-						for( j=1; j<columns-1; j++ )
-							accessMat( surfaceCopy, i, j ) = accessMat( surface, i, j );
-				}
-
 				/* 4.2.3. Update surface values (skip borders) */
-				//#pragma parallel loop
-				//{
-					for( i=1; i<rows-1; i++ )
-						for( j=1; j<columns-1; j++ )
-							accessMat( surface, i, j ) = (
-								accessMat( surfaceCopy, i-1, j ) +
-								accessMat( surfaceCopy, i+1, j ) +
-								accessMat( surfaceCopy, i, j-1 ) +
-								accessMat( surfaceCopy, i, j+1 ) ) / 4;
-				//}
+				//#pragma acc data copyout(surface[:]) copyin(surfaceCopy[:])
+				#pragma acc loop seq
+				for( i=1; i<rows-1; i++ ) {
+					#pragma acc loop gang vector
+					for( j=1; j<columns-1; j++ )
+					{
+						accessMat( surface, i, j ) = ( 
+							accessMat( surfaceCopy, i-1, j ) +
+							accessMat( surfaceCopy, i+1, j ) +
+							accessMat( surfaceCopy, i, j-1 ) +
+							accessMat( surfaceCopy, i, j+1 ) ) / 4;
+					}
+				}
 			}
-
 			/* 4.2.4. Compute the maximum residual difference (absolute value) */
-			//#pragma acc loop
-			//{
-				global_residual = 0.0f;
+			if (num_deactivated == num_focal)
+				flag_stability = 1;
+				//#pragma acc data copyin(surface,surfaceCopy)
+				#pragma acc loop seq
 				for( i=1; i<rows-1; i++ )
 					for( j=1; j<columns-1; j++ )
-						if ( fabs( accessMat( surface, i, j ) - accessMat( surfaceCopy, i, j ) ) > global_residual ) {
-							global_residual = fabs( accessMat( surface, i, j ) - accessMat( surfaceCopy, i, j ) );
+						/* If the global residual is lower than THRESHOLD, we have reached enough stability, stop simulation at the end of this iteration */
+						if ( fabs( accessMat( surface, i, j ) - accessMat( surfaceCopy, i, j ) ) > THRESHOLD) {
+							flag_stability = 0;
+							break;
 						}
-			//}
 		}
-
-			/* If the global residual is lower than THRESHOLD, we have reached enough stability, stop simulation at the end of this iteration */
-		if( num_deactivated == num_focal && global_residual < THRESHOLD ) flag_stability = 1;
-
 		/* 4.3. Move teams */
+		#pragma acc loop seq
 		for( t=0; t<num_teams; t++ ) {
 			/* 4.3.1. Choose nearest focal point */
-			float distance = FLT_MAX;
-			int target = -1;
+			distance = FLT_MAX;
+			target = -1;
+			#pragma acc loop seq
 			for( j=0; j<num_focal; j++ ) {
 				if ( focal[j].active != 1 ) continue; // Skip non-active focal points
-				float dx = focal[j].x - teams[t].x;
-				float dy = focal[j].y - teams[t].y;
-				float local_distance = sqrtf( dx*dx + dy*dy );
+				dx = focal[j].x - teams[t].x;
+				dy = focal[j].y - teams[t].y;
+				local_distance = sqrtf( dx*dx + dy*dy );
 				if ( local_distance < distance ) {
 					distance = local_distance;
 					target = j;
@@ -386,19 +398,17 @@ int main(int argc, char *argv[]) {
 			}
 			/* 4.3.2. Annotate target for the next stage */
 			teams[t].target = target;
-
 			/* 4.3.3. No active focal point to choose, no movement */
-			if ( target == -1 ) continue;
-
+			if ( target == -1 ) continue; 
 			/* 4.3.4. Move in the focal point direction */
-			if ( teams[t].type == 1 ) {
+			if ( teams[t].type == 1 ) { 
 				// Type 1: Can move in diagonal
 				if ( focal[target].x < teams[t].x ) teams[t].x--;
-				if ( focal[target].x > teams[t].x ) teams[t].x++;
+				else if ( focal[target].x > teams[t].x ) teams[t].x++;
 				if ( focal[target].y < teams[t].y ) teams[t].y--;
-				if ( focal[target].y > teams[t].y ) teams[t].y++;
+				else if ( focal[target].y > teams[t].y ) teams[t].y++;
 			}
-			else if ( teams[t].type == 2 ) {
+			else if ( teams[t].type == 2 ) { 
 				// Type 2: First in horizontal direction, then in vertical direction
 				if ( focal[target].y < teams[t].y ) teams[t].y--;
 				else if ( focal[target].y > teams[t].y ) teams[t].y++;
@@ -413,38 +423,42 @@ int main(int argc, char *argv[]) {
 				else if ( focal[target].y > teams[t].y ) teams[t].y++;
 			}
 		}
-
 		/* 4.4. Team actions */
+		//#pragma acc loop seq
 		for( t=0; t<num_teams; t++ ) {
 			/* 4.4.1. Deactivate the target focal point when it is reached */
-			int target = teams[t].target;
-			if ( target != -1 && focal[target].x == teams[t].x && focal[target].y == teams[t].y 
+			target = teams[t].target;
+			if ( target != -1 
+				&& focal[target].x == teams[t].x 
+				&& focal[target].y == teams[t].y 
 				&& focal[target].active == 1 )
 				focal[target].active = 2;
-
 			/* 4.4.2. Reduce heat in a circle around the team */
-			int radius;
 			// Influence area of fixed radius depending on type
+			radius = RADIUS_TYPE_2_3;
 			if ( teams[t].type == 1 ) radius = RADIUS_TYPE_1;
-			else radius = RADIUS_TYPE_2_3;
+			//#pragma acc loop seq
 			for( i=teams[t].x-radius; i<=teams[t].x+radius; i++ ) {
+				//#pragma acc loop seq
 				for( j=teams[t].y-radius; j<=teams[t].y+radius; j++ ) {
 					if ( i<1 || i>=rows-1 || j<1 || j>=columns-1 ) continue; // Out of the heated surface
-					float dx = teams[t].x - i;
-					float dy = teams[t].y - j;
-					float distance = sqrtf( dx*dx + dy*dy );
+					dx = teams[t].x - i;
+					dy = teams[t].y - j;
+					distance = sqrtf( dx*dx + dy*dy );
 					if ( distance <= radius ) {
 						accessMat( surface, i, j ) = accessMat( surface, i, j ) * ( 1 - 0.25 ); // Team efficiency factor
 					}
 				}
 			}
-		}
 
-#ifdef DEBUG
+		}
+	#ifdef DEBUG
 		/* 4.5. DEBUG: Print the current state of the simulation at the end of each iteration */
 		print_status( iter, rows, columns, surface, num_teams, teams, num_focal, focal, global_residual );
-#endif // DEBUG
+	#endif // DEBUG
 	}
+	iter_total = iter;
+
 
 /*
  *
@@ -460,7 +474,7 @@ int main(int argc, char *argv[]) {
 	/* 6.1. Total computation time */
 	printf("Time: %lf\n", ttotal );
 	/* 6.2. Results: Number of iterations, residual heat on the focal points */
-	printf("Result: %d", iter);
+	printf("Result: %d", iter_total);
 	for (i=0; i<num_focal; i++) {
 		int x = focal[i].x;
 		int y = focal[i].y;
@@ -469,7 +483,7 @@ int main(int argc, char *argv[]) {
 	}
 	printf("\n");
 
-	/* 7. Free resources */
+	/* 7. Free resources */	
 	free( teams );
 	free( focal );
 	free( surface );
@@ -478,5 +492,3 @@ int main(int argc, char *argv[]) {
 	/* 8. End */
 	return 0;
 }
-
-
